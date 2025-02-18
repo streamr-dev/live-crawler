5de6ae65daa8ae6b1614ea0e49053a5242050d98
Eric Andrews <harbu88@hotmail.com>
2025-02-18 11:09:21 +0200

    add simple caching to location fetching

--- src/fetchLocationData.ts
+++ src/fetchLocationData.ts
@@ -12,14 +12,22 @@ interface LocationData {
     timezone?: string
 }
 
+// TODO: clean up cache
+const locationCache = new Map<string, LocationData>()
+
 export async function fetchLocationData(ipAddress: string): Promise<LocationData> {
+    const cachedData = locationCache.get(ipAddress)
+    if (cachedData !== undefined) {
+        return cachedData
+    }
+
     try {
         const response = await fetch(`https://ipinfo.io/${ipAddress}?token=29de457f326044`)
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`)
         }
         const data = await response.json() as any
-        return {
+        const locationData = {
             city: data?.city,
             country: data?.country,
             loc: data?.loc,
@@ -28,6 +36,9 @@ export async function fetchLocationData(ipAddress: string): Promise<LocationData
             postal: data?.postal,
             timezone: data?.timezone,
         }
+
+        locationCache.set(ipAddress, locationData)
+        return locationData
     } catch (err) {
         logger.warn('Failed to fetch location data', { err, ipAddress })
         return {}
