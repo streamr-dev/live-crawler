4ae955a26be3359e42942bcd37cec8637022b051
Eric Andrews <harbu88@hotmail.com>
2025-02-19 13:05:03 +0200

    chore: use @streamr/geoip-location instead of HTTP calls

--- src/fetchLocationData.ts
+++ src/fetchLocationData.ts
@@ -1,7 +1,7 @@
+import { GeoIpLocator } from '@streamr/geoip-location'
 import { Logger } from '@streamr/utils'
 import iso3166Data from './iso-3166-data.json'
-
-const logger = new Logger(module)
+import { CityResponse } from "mmdb-lib/lib/reader/response"
 
 interface LocationData {
     city?: string
@@ -14,38 +14,36 @@ interface LocationData {
     subRegion?: string
 }
 
+const logger = new Logger(module)
+
+const locator = new GeoIpLocator('~/geoipdatabases')
+
+locator.start().catch((err) => {
+    logger.fatal('Failed to start geoip locator', { err })
+    process.exit(1)
+})
+
 const countryToSubRegionMap = new Map(
     iso3166Data.map((country: any) => [country['alpha-2'], country['sub-region']])
 )
 
-// TODO: clean up cache
-const locationCache = new Map<string, LocationData>()
-
 export async function fetchLocationData(ipAddress: string): Promise<LocationData> {
-    const cachedData = locationCache.get(ipAddress)
-    if (cachedData !== undefined) {
-        return cachedData
-    }
-
     try {
-        const response = await fetch(`https://ipinfo.io/${ipAddress}?token=29de457f326044`)
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`)
+        // @ts-expect-error accesing priavte property
+        const location: CityResponse = locator.reader.get(ipAddress)
+        if (location === undefined) {
+            return {}
         }
-        const data = await response.json() as any
-        const locationData = {
-            city: data?.city,
-            country: data?.country,
-            loc: data?.loc,
-            hostname: data?.hostname,
-            org: data?.org,
-            postal: data?.postal,
-            timezone: data?.timezone,
-            subRegion: data?.country !== undefined ? countryToSubRegionMap.get(data.country) : undefined
+        return {
+            city: location.city?.names?.en,
+            country: location?.country?.names?.en,
+            loc: location.location?.latitude + ',' + location.location?.longitude,
+            hostname: location.traits?.domain,
+            org: location.traits?.organization ?? location.traits?.isp,
+            postal: location.postal?.code,
+            timezone: location.location?.time_zone,
+            subRegion: countryToSubRegionMap.get(location.country?.iso_code)
         }
-
-        locationCache.set(ipAddress, locationData)
-        return locationData
     } catch (err) {
         logger.warn('Failed to fetch location data', { err, ipAddress })
         return {}
